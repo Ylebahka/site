2a55beeffd7f7fe78399bb3dfcecb08f10fa93f9
Ylebahka <ylebahka@gmail.com>
2023-05-15 22:18:58 +0200

    login Menu

---
+++ script.js
@@ -0,0 +1,9 @@
+var Clicked = false;
+document.querySelector("#NavButton").addEventListener("click", function(){
+   
+       
+        
+  
+     document.querySelector(".LoginMenu").classList.toggle("LoginMenu-View")  
+
+});

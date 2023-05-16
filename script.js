fef0a21de96c93e3152564cb714071c98a17b70d
Vova <walk.up.myself@gmail.com>
2023-05-16 15:02:16 +0200

    VovaPC

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-var Clicked = false;
 document.querySelector("#NavButton").addEventListener("click", function(){
-   
-       
-        
-  
      document.querySelector(".LoginMenu").classList.toggle("LoginMenu-View")  
-
-});
+})
+document.querySelector(".RegistrationSelectButton").addEventListener("click", function(){
+     document.querySelector("#LoginInputMenu").classList.toggle("LoginInputMenu-deactive")
+     document.querySelector("#RegistrationInputMenu").classList.toggle("RegistrationInputMenu-active")
+     document.querySelector(".LoginSelectButton").classList.toggle("LoginChosen")
+     document.querySelector(".RegistrationSelectButton").classList.toggle("RegistrationChosen")
+})
+document.querySelector(".LoginSelectButton").addEventListener("click", function(){
+     document.querySelector("#LoginInputMenu").classList.toggle("LoginInputMenu-deactive")
+     document.querySelector("#RegistrationInputMenu").classList.toggle("RegistrationInputMenu-active")
+     document.querySelector(".LoginSelectButton").classList.toggle("LoginChosen")
+     document.querySelector(".RegistrationSelectButton").classList.toggle("RegistrationChosen")
+})
